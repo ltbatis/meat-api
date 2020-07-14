b7aae447a1e746afe09f173e2ffd9c2f54c58a4d
ltbatista <ltbatista30@gmail.com>
2020-07-13 23:44:11 -0700

    adding post method

--- users/users.router.ts
+++ users/users.router.ts
@@ -24,6 +24,15 @@ class UsersRouter extends Router {
       })
     })
 
+    application.post('/users', (req, resp, next)=>{
+      let user = new User(req.body)
+      user.save().then(user=>{
+        user.password = undefined
+        resp.json(user)
+        return next()
+      })
+    })
+
   }
 }
 
