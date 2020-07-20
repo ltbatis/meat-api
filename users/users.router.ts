48d779c416202638ed8200ae18a7b458e039c9dc
ltbatista <ltbatista30@gmail.com>
2020-07-20 12:23:57 -0700

    dependabot suggestion

--- users/users.router.ts
+++ users/users.router.ts
@@ -62,7 +62,7 @@ class UsersRouter extends Router {
     application.del('/users/:id', (req, resp, next)=>{
       User.remove({_id:req.params.id}).exec().then((cmdResult: any)=>{
         if(cmdResult.result.n){
-          resp.send(204)          
+          resp.send(204)
         }else{
           resp.send(404)
         }
