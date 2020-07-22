60136904487324cbb97c39c72fd1ead9409951a2
ltbatista <ltbatista30@gmail.com>
2020-07-21 18:00:46 -0700

    validating CPF, custom validate

--- dist/users/users.model.js
+++ dist/users/users.model.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.User = void 0;
 const mongoose = require("mongoose");
+const validators_1 = require("../common/validators");
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -24,6 +25,14 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: false,
         enum: ['Male', 'Female', 'LGBTQI+']
+    },
+    cpf: {
+        type: String,
+        required: false,
+        validate: {
+            validator: validators_1.validateCPF,
+            message: '{PATH}: Invalid CPF ({VALUE})'
+        }
     }
 });
 exports.User = mongoose.model('User', userSchema);
